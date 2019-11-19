b90b0bbcddc4ca9d177f8970c26c71747074c9e9
Josephat-n <josphatnjoroge254@gmail.com>
2019-11-19 12:13:47 +0300

    create a class article and pass parameters for the article objects.

---
+++ main/models/NewsArticle.py
@@ -0,0 +1,12 @@
+class Article:
+   """
+   The blueprint for article objects.
+   """
+   
+   def __init__(self,source, title, description, url, urlToImage,publishedAt)
+      self.source = source
+      self.title = title
+      self.description = description
+      self.url = url
+      self.urlToImage = urlToImage
+      self.publishedAt = publishedAt
