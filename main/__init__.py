1af1bde25434823b3c82acc1b632405fc581a8a3
Josephat-n <josphatnjoroge254@gmail.com>
2019-11-20 00:02:00 +0300

    add bootstrap to our application

--- main/__init__.py
+++ main/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from .config import DevConfig
+from flask_bootstrap import Bootstrap
 
 #Initialize the app
 app = Flask(__name__,instance_relative_config = True)
@@ -7,5 +8,8 @@ app = Flask(__name__,instance_relative_config = True)
 #The configuration settings
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
+#Initialize Bootstrap
+bootstrap = Bootstrap(app)
 
 from main import views
