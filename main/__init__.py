19ee2699c21505b4bd5d72773bcef2e84ee54d55
Josephat-n <josphatnjoroge254@gmail.com>
2019-11-19 11:19:02 +0300

    introduce an api key and code to allw us to access the key in app.

--- main/__init__.py
+++ main/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from .config import DevConfig
 
 #Initialize the app
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config = True)
+
+#The configuration settings
+app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 
 from main import views
