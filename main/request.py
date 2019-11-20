3f768aa6179149daf0620212aec63d4e7a03a200
Josephat-n <josphatnjoroge254@gmail.com>
2019-11-20 22:05:29 +0300

    create a file to display news article when the link to the news-source is clicked

--- main/request.py
+++ main/request.py
@@ -12,7 +12,7 @@ api_key=app.config['NEWS_API_KEY']
 source_url = app.config["SOURCE_URL"]
 articles_url= app.config['ARTICLES_URL']
 
-def get_sources(category):
+def get_sources():
    """
    get sources using the json response
    """
@@ -28,32 +28,32 @@ def get_sources(category):
          source_results_list = source_response['sources']
          source_results = process_results(source_results_list)
 
-      return source_results 
+   return source_results 
 
 def process_results(source_list):
    """
    Transform the article results into a list of objects.
    """
-    source_results=[]
+   source_results=[]
 
-    for news_item in source_list:
-        id=news_item.get('id')
-        name=news_item.get('name')
-        description=news_item.get('description')
-        url=news_item.get('url')
-        category=news_item.get('category')
-        country=news_item.get('country')
-                   
-        source_object = Source(id,name,description,url,category,country)
-        source_results.append(source_object)
+   for news_item in source_list:
+      id=news_item.get('id')
+      name=news_item.get('name')
+      description=news_item.get('description')
+      url=news_item.get('url')
+      category=news_item.get('category')
+      country=news_item.get('country')
+                  
+      source_object = Source(id,name,description,url,category,country)
+      source_results.append(source_object)
 
-    return source_results   
+   return source_results   
 
 def get_articles(id):
    """
    get articles using the json response
    """
-   get_article_url = articles_url.format(api_key)
+   get_article_url = articles_url.format(id,api_key)
    
    with urllib.request.urlopen(get_article_url) as url:
       get_articles_data = url.read()
@@ -61,8 +61,8 @@ def get_articles(id):
       
       article_results = None  
       
-      if get_articles_resp['sources']:
-        article_results_list = get_articles_resp['sources']
+      if get_articles_resp['articles']:
+        article_results_list = get_articles_resp['articles']
         article_results= process_articles(article_results_list)
         
    return article_results
@@ -80,7 +80,7 @@ def process_articles(article_list):
       urlToImage =  article_item.get('urlToImage') 
       publishedAt =  article_item.get('publishedAt')
       
-      article_object = Article(source,title,description,url,urlToImage,publishedAt)
-      
+      new_article = Article(source,title,description,url,urlToImage,publishedAt)
+      article_results.append(new_article)
       
    return article_results     
--- main/views.py
+++ main/views.py
@@ -1,16 +1,22 @@
 from flask import render_template
 from main import app
-from .request import get_articles
+from .request import get_articles, get_sources
 
 #This defines the various views in our app
 @app.route('/')
 def index():
    """
    function to return the index page and its resources
-   """
-    
-   # Displaying the news Articles
-   dispArticle = get_articles()
-   print(dispArticle)
+   """    
+   # Displaying the news Sources
+   display_sources = get_sources()
    
-   return render_template('index.html', articles = dispArticle)
+   return render_template('index.html', sourcess = display_sources)
+
+@app.route('/articles/<id>')
+def source(id):
+       
+    display_articles = get_articles(id)
+    print(display_articles)
+     
+    return render_template('articles.html',id=id, articles = display_articles)
