34fdff8561d2817cf3f3226e2b62ffd07ce26fc4
Josephat-n <josphatnjoroge254@gmail.com>
2019-11-21 01:32:25 +0300

    update news article test cases

--- main/models/NewsArticle.py
+++ main/models/NewsArticle.py
@@ -2,6 +2,7 @@ class Article:
    """
    The blueprint for article objects.
    """
+   article_results = [] #An array of all articles
    
    def __init__(self,source, title, description, url, urlToImage,publishedAt):
       self.source = source
@@ -10,3 +11,6 @@ class Article:
       self.url = url
       self.urlToImage = urlToImage
       self.publishedAt = publishedAt
+      
+   def save_article(self):
+      Article.article_results.append(self)   
---
+++ main/test_article.py
@@ -0,0 +1,25 @@
+import unittest
+from models import NewsArticle
+Article = NewsArticle.Article
+
+class ArticleTest(unittest.TestCase):
+   """
+   Test the behaviour of News class
+   """
+
+   def setUp(self):
+      """
+      Set up method that will run before every Test
+      """
+      self.new_article = Article("abc-news","ABC_News","Your trusted source for breaking news","https://abcnews.go.com", "general","us")
+
+   def test_instance(self):
+      self.assertTrue(isinstance(self.new_article,Article))
+        
+   def test_save_source(self):
+      self.new_article.save_source()  # saving the new source
+      self.assertEqual(len(Article.source_results),1)  
+
+
+if __name__ == '__main__':
+    unittest.main()
