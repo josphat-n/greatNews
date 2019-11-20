336d9bf5d537b8d01e3d4f5f491c9d2b13c0d677
Josephat-n <josphatnjoroge254@gmail.com>
2019-11-20 15:32:54 +0300

    create a News-Source class, instantiate it and call an instance in request.py file

---
+++ main/models/NewsSource.py
@@ -0,0 +1,11 @@
+class Source:
+   """
+   The blueprint for News-source objects.
+   """      
+   def __init__(self, id, name, description, url, category, country):
+      self.id = id
+      self.name = name
+      self.description = description
+      self.url = url
+      self.category = category
+      self.country = country
--- main/request.py
+++ main/request.py
@@ -1,8 +1,9 @@
 from main import app
 import urllib.request,json
-from .models import NewsArticle
+from .models import NewsArticle, NewsSource
 
 Article = NewsArticle.Article
+Source = NewsSource.Source
 
 #Get the api key
 api_key=app.config['NEWS_API_KEY']
@@ -12,7 +13,7 @@ base_url = app.config["NEWS_API_BASE_URL"]
 
 def get_articles():
    """
-   get articles using the json response
+   get sources using the json response
    """
    get_article_url = base_url.format(api_key)
    
@@ -22,8 +23,8 @@ def get_articles():
       
       article_results = None  
       
-      if get_articles_resp['articles']:
-        article_results_list = get_articles_resp['articles']
+      if get_articles_resp['sources']:
+        article_results_list = get_articles_resp['sources']
         article_results= process_articles(article_results_list)
         
    return article_results
@@ -34,15 +35,23 @@ def process_articles(article_list):
    """
    article_results = []
    for article_item in article_list:
-      source = article_item.get('source')
-      title =  article_item.get('title')
-      description =  article_item.get('description')
-      url =  article_item.get('url') 
-      urlToImage =  article_item.get('urlToImage') 
-      publishedAt =  article_item.get('publishedAt')
+      # source = article_item.get('source')
+      # title =  article_item.get('title')
+      # description =  article_item.get('description')
+      # url =  article_item.get('url') 
+      # urlToImage =  article_item.get('urlToImage') 
+      # publishedAt =  article_item.get('publishedAt')
       
-      article_object = Article(source,title,description,url,urlToImage,publishedAt)
+      # article_object = Article(source,title,description,url,urlToImage,publishedAt)
       
+      id = article_item.get('id')
+      name = article_item.get('name')
+      description = article_item.get('description')
+      url = article_item.get('url')
+      category = article_item.get('category')
+      country = article_item.get('country')
+      
+      article_object = Source(id,name, description,url,category,country)
       article_results.append(article_object)     
    
    return article_results     
