a3a95c8fa907795e71f1441feb72e0ec611baad7
Josephat-n <josphatnjoroge254@gmail.com>
2019-11-21 01:12:57 +0300

    update testcases for the news sources

--- main/models/NewsSource.py
+++ main/models/NewsSource.py
@@ -2,6 +2,8 @@ class Source:
    """
    The blueprint for News-source objects.
    """      
+   source_results = [] #An empty array of news sources
+   
    def __init__(self, id, name, description, url, category, country):
       self.id = id
       self.name = name
@@ -9,3 +11,7 @@ class Source:
       self.url = url
       self.category = category
       self.country = country
+      
+   def save_source(self):
+      Source.source_results.append(self)
+      
---
+++ main/test_source.py
@@ -0,0 +1,25 @@
+import unittest
+from models import NewsSource
+Source = NewsSource.Source
+
+class NewsTest(unittest.TestCase):
+    """
+    Test the behaviour of News class
+    """
+
+    def setUp(self):
+        """
+        Set up method that will run before every Test
+        """
+        self.new_source = Source("abc-news","ABC_News","Your trusted source for breaking news","https://abcnews.go.com", "general","us")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source,Source))
+        
+    def test_save_source(self):
+        self.new_source.save_source()  # saving the new source
+        self.assertEqual(len(Source.source_results),1)  
+
+
+if __name__ == '__main__':
+    unittest.main()
