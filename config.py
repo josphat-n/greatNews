d35d238c69ed8a54db48b8316cd17db872c90e54
Josephat-n <josphatnjoroge254@gmail.com>
2019-11-21 10:50:01 +0300

    update the source URL to fetch news in english language.

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
    #This is the url for all named sources available in the News API website
-   SOURCE_URL = 'https://newsapi.org/v2/sources?apiKey={}'   
+   SOURCE_URL = 'https://newsapi.org/v2/sources?language=en&apiKey={}'
    #URL for fetching the news articles.
    ARTICLES_URL='https://newsapi.org/v2/everything?sources={}&apikey={}'
    
